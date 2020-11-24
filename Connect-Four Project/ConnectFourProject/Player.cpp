ed38190b413ad415f9b2079ecdd73d4ee7d032aa
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2020-11-24 18:42:52 +0000

    Modified Player.h and Player.cpp
    Moved methods from Player.h to Player.cpp to resolve file structure issue

--- Connect-Four Project/ConnectFourProject/Player.cpp
+++ Connect-Four Project/ConnectFourProject/Player.cpp
@@ -1,1 +1,42 @@
 #include "Player.h"
+
+
+// Method -- Assign user to either X or O
+void Player::SetPlayerSymbol(Player& PlayerObject1, Player& PlayerObject2) {
+
+	// Store the user's choice in this temporary variable to carry out error handling
+	char Temp;
+
+	// Get user's input
+	cout << "Player 1 gets first choice! Pick your Symbol (X or O): ";
+	cin >> Temp;
+
+	// Convert to Uppercase
+	Temp = toupper(Temp);
+
+	// Make sure that user only chooses X or O and nothing else
+	while (Temp != 79 && Temp != 88)
+	{
+		// Get user's input
+		cout << "\nWrong value, try again. Pick your Symbol (X or O): ";
+		cin >> Temp;
+
+		// Convert to Uppercase
+		Temp = toupper(Temp);
+	}
+
+	if (Temp == 'O')
+	{
+		PlayerObject1.UpdateChosenSymbol("O");
+		PlayerObject2.UpdateChosenSymbol("X");
+	}
+
+	else
+	{
+		PlayerObject1.UpdateChosenSymbol("X");
+		PlayerObject2.UpdateChosenSymbol("O");
+	}
+
+
+
+}
